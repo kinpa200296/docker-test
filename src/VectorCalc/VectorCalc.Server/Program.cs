05493f35a40237cd7a1870478e7a87efa041e013
Pavel Kindruk <kinpa200296@yandex.ru>
2018-10-31 02:56:29 +0300

    Added sample app.

---
+++ src/VectorCalc/VectorCalc.Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VectorCalc.Server
+{
+    class Program
+    {
+        const int Port = 0xC0DE;
+
+        static void Main(string[] args)
+        {
+            var listener = new TcpListener(IPAddress.Loopback, Port);
+            listener.Start();
+            Console.WriteLine($"Server started on port {Port}");
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+
+                var clientStream = client.GetStream();
+
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+                var bytesRead = clientStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                Console.WriteLine($"Recived: [{string.Join(",", buffer.Take(bytesRead))}]");
+
+                //System.Threading.Thread.Sleep(5000);
+
+                clientStream.Write(buffer, 0, bytesRead);
+
+                client.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
